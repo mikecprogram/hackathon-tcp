34ccc206f024b4b880930f7e060ca55be3a68243
mikecprogram <41227864+mikecprogram@users.noreply.github.com>
2021-12-22 19:21:43 +0200

    Client half baked

---
+++ client.py
@@ -0,0 +1,88 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import msvcrt
+import sys
+from socket import *
+
+TEAMNAME = 'CM'
+TXT_ENCODING = 'utf-8'
+NUM_ENCODING = "little"
+CLIENT_STARTED_MSG = 'Client started, listening for offer requests...'
+RCVD_OFFER_MSG = 'Received offer from {},\nattempting to connect...'
+IMPOSTER_OFFER_MSG = 'An imposter server ({})tried to connect, but it had failed.'
+FAILED_TO_CONNECT_MSG = 'Failed to connect'
+MAGIC_COOKIE = b'\xab\xcd\xdc\xba'
+MSG_TYPE_OFFER = b'\x02'
+BUFFER_SIZE = 1024
+UDP_PORT = 13117
+
+
+def looking_for_server_state():
+    print(CLIENT_STARTED_MSG)
+    try:
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        s.bind(('', UDP_PORT))
+        m = s.recvfrom(BUFFER_SIZE)
+        receivedbytes = m[0]
+        serverip = m[1][0]
+        if receivedbytes[0:5] ==  b'\xab\xcd\xdc\xba\x02':
+            receivedport = int.from_bytes(receivedbytes[5:7], NUM_ENCODING)
+            print(RCVD_OFFER_MSG.format(serverip))
+            return serverip, receivedport
+        else:
+            print(IMPOSTER_OFFER_MSG.format(serverip))
+            return looking_for_server_state()
+    except Exception as e:
+        print(e)
+
+    return None, None
+
+
+def connect_to_server_state(serverip, port):
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((serverip, port))
+        return s
+    except Exception as e:
+        print(e)
+    return None
+
+def read(t):
+    try:
+        data = t.recv(BUFFER_SIZE)
+        if data!= "":
+            data = str(data, TXT_ENCODING)
+            print(data)
+    except:
+        pass
+
+def gamemode(tcp):
+    data = ""
+    print("connected!")
+    while data != "end":
+        read(tcp)
+        if msvcrt.kbhit():
+            tcp.send(msvcrt.getch())
+    return 0
+
+
+def theloop():
+    (serverip, port) = looking_for_server_state()
+    tcpsocket = connect_to_server_state(serverip, port)
+    if tcpsocket == None:
+        print(FAILED_TO_CONNECT_MSG)
+        theloop()
+    else:
+        tcpsocket.send(bytes(TEAMNAME, TXT_ENCODING))
+        gamemode(tcpsocket)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #theloop()
+    msvcrt.putch(b'\x77')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
